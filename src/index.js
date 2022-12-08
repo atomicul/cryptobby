86b130a864e8a598662ada78540837740f2a1a8a
atomicul <soaremihai2006@gmail.com>
2022-12-08 23:51:17 +0200

    Fix routing in gh pages I think?

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <HashRouter >
+        <App />
+    </HashRouter>
 );
